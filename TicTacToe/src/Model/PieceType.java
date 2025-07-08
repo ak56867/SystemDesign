1f73a30af27ad66eb42e1267f861f06f5abd889f
abhinav <62859555+ak56867@users.noreply.github.com>
2025-07-08 19:43:07 +0530

    Delete TicTacToe directory

--- TicTacToe/src/Model/PieceType.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public enum PieceType {
-
-    //Types of pieces we can use, more types can be added as per requirement
-    X,
-    O;
-}
--- TicTacToe/src/Model/PlayingPiece.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class PlayingPiece {
-    PieceType pieceType;
-
-    PlayingPiece(PieceType pieceType){
-        this.pieceType = pieceType;
-    }
-}
